c32fd96f8b3cec89e626c00c6aa51fee663baa39
JustACommentator <t.glasmacher@aol.com>
2022-03-28 16:25:03 +0200

    M1 Aufgabe 10

--- app/Http/Controllers/AbaloController.php
+++ app/Http/Controllers/AbaloController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AbArticle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AbaloController extends Controller
 {
     public function search() {
 
+        $search = $_GET['search']??null;
         $results = array();
-        return view('searchView', ["search" => $_GET['search']??Null, "results" => $results]);
+        if($search != null) {
+            $results = DB::table('article_creator as ac')->select(
+                'id', 'article_name', 'article_price', 'ab_createdate', 'seller')
+                ->where("article_name", "ilike", "%$search%")->get();
+        }
+        return view('searchView', ["search" => $_GET['search']??null, "results" => $results]);
     }
 }
--- resources/views/searchView.blade.php
+++ resources/views/searchView.blade.php
@@ -1,10 +1,11 @@
 <form method="GET" action="articles">
-    <input type="text" name="search">
+    <input type="text" name="search" @if($_GET['search'] != null) value="{{$_GET['search']}}" @else placeholder="Suchbegriff" @endif>
     <input type="submit">
 </form>
 
 <table>
     <tr>
+        <th>Bild</th>
         <th>Artikel</th>
         <th>Preis</th>
         <th>VerkäuferID</th>
@@ -14,9 +15,15 @@
     @foreach($results as $result)
 
         <tr>
-            <td>{{$result->ab_name}}</td>
-            <td>{{$result->ab_price}}</td>
-            <td>{{$result->ab_creator_id}}</td>
+            <td>
+
+                @if(file_exists("assets/img/$result->id.jpg"))
+                    <img src="assets/img/{{$result->id}}.jpg">
+                    @endif
+            </td>
+            <td>{{$result->article_name}}</td>
+            <td>{{$result->article_price}}</td>
+            <td>{{$result->seller}}</td>
             <td>{{$result->ab_createdate}}</td>
         </tr>
 
