1bba3ec6ea37d46d3220aa14e81d13241caf6909
JustACommentator <t.glasmacher@aol.com>
2022-03-28 13:28:53 +0200

    M1 Aufgabe 5 (Optional)

---
+++ app/Http/Controllers/AbTestDataController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AbTestData;
+use Illuminate\Http\Request;
+
+class AbTestDataController extends Controller
+{
+    public function testMethod() {
+        foreach(AbTestData::query()->select()->get() as $item) {
+            echo $item->id . ", " . $item->ab_testname . "<br>";
+        }
+    }
+}
